ef0b08ec9ddd182adf3bb5972aacc56e2be0c622
DeanDal1000 <dalzelldean@gmail.com>
2021-01-07 13:21:05 +0000

    First Commit

---
+++ src/pages/contact.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Contact = () => {
+    return (  <div>Number Here</div>);
+}
+ 
+export default Contact;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,9 @@
-import React from "react"
+import React from 'react';
 
-export default function Home() {
-  return <div>Hello world!</div>
+const IndexPage = () => {
+  return (
+    <div>Hello Im dean</div>
+  )
 }
+
+export default IndexPage
