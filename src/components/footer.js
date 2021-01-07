f6d57e129b42ac251fd2e9fcd9c83a12e2e325bc
DeanDal1000 <dalzelldean@gmail.com>
2021-01-07 14:15:37 +0000

    Links

---
+++ src/components/footer.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Footer = () => {
+    return ( <footer><p>Hello Dean</p></footer> );
+}
+ 
+export default Footer;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,14 @@
 import React from 'react';
+import { Link } from 'gatsby'
+import Footer from '../components/footer'
 
 const IndexPage = () => {
   return (
-    <div>Hello Im dean</div>
+    <div>Hello Im dean
+
+      <p><Link to="/contact">Contact Me</Link></p>
+      <Footer />
+    </div>
   )
 }
 
